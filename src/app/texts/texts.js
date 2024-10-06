ee15ac5e23a742681bed69d42eef27bb08dee41d
kevinpicon01 <digitalpruu@gmail.com>
2024-10-06 14:10:41 -0500

    fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,4 +1,4 @@
-const title = "Confecciones Isabella";
+const title = "Derpol";
 const email = "astrid_27_26@hotmail.com";
 const mensajeWhatsApp = encodeURIComponent(
     "¡Hola! Estoy interesado en conocer más sobre los productos de " + title + " y me gustaría obtener más información. ¡Gracias!"
@@ -13,7 +13,7 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "En Confecciones Isabella, nos dedicamos a la confección de sudaderas y buzos que te harán sentir cómodo y con estilo. Nuestro lema 'Vístete como si fuera tu día' refleja nuestro compromiso con la moda y la calidad en cada prenda.",
+        description1: "En Derpol, nos dedicamos a la confección de sudaderas y buzos que te harán sentir cómodo y con estilo. Nuestro lema 'Vístete como si fuera tu día' refleja nuestro compromiso con la moda y la calidad en cada prenda.",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
